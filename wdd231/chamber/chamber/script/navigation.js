e196de16b0f77f2a3e67e9d553381185a68d413b
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-03-16 10:29:31 +0200

    updated wdd231

--- wdd231/chamber/chamber/script/navigation.js
+++
@@ -1,7 +0,0 @@
-// Responsive Navigation Menu
-const menuButton = document.getElementById('menu-button');
-const navList = document.querySelector('nav ul');
-
-menuButton.addEventListener('click', () => {
-    navList.classList.toggle('active');
-});
