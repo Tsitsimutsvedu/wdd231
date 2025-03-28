ad12af62705f2eb6ce432c0833e2c381615f1765
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-03-28 19:40:16 +0200

    wdd231

--- chamber-homepage/chamber-homepage/js/date.js
+++
@@ -1,6 +0,0 @@
-// Dynamic Dates in Footer
-const currentYear = new Date().getFullYear();
-document.getElementById('current-year').textContent = currentYear;
-
-const lastModified = document.lastModified;
-document.getElementById('lastModified').textContent = `Last Updated: ${lastModified}`;
